0fab9a771385a061d453aff7382278967239dd7b
Khanyi20 <251301@virtualwindow.co.za>
2026-03-15 16:34:39 +0200

    First changes for the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import'./images/Asset 1.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Nav } from 'react-bootstrap';
+
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Image from "./pages/VillainImage";
+import brand from "./images/Asset 1.svg";
+//import Image from "./pages/VillainImage";
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+
+    
+    <BrowserRouter>
+    <div className="img-background">
+      
+
+    
+    
+
+      <Nav defaultActiveKey="/" className="flex-column">
+      
+        <img src={brand} style={{backgroundImage: "url(./images/Asset 1.svg) ", height:"65%" , width:"65%"}} alt='No image found/Error'/>
+      
+      
+  <Link to="/"  ><button className="nav-name">Home</button></Link>
+  <Link to="about"  style = {{marginTop: '10px'}}><button className="nav-name">About</button></Link>
+  <Link to="contact"  style = {{marginTop: '10px'}}><button className="nav-name">Contact</button></Link>
+  <Link to="image"  style = {{marginTop: '10px'}}><button className="nav-name">Image</button></Link>
+</Nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/image" element={<Image />} />
+
+      </Routes>
+      </div>
+
+       
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,93 @@
+// import { useState, useEffect } from "react";
+// import Image from 'react-bootstrap/Image'
+// import { Container, Form } from "react-bootstrap";
+// import { Row} from "react-bootstrap";
+// import { Col } from "react-bootstrap";
+//import brand from './src/Asset 1.svg'
+
+import React from "react";
+// import'./Asset 1.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from "react-bootstrap";
+import first from './';
+//import Carousel from 'react-bootstrap/Carousel';
+
+
+
+
+function Home() {
+  return (
+    <div className="content">
+
+      <div className="carousel">
+      <Carousel fade>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={first} alt="Company Logo"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Second slide&bg=282c34"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+      </div>
+    
+        <div  className="container">
+      <input type="text" placeholder="Search" className="search-area" />
+      <button className="search-button">Search</button>
+      </div>
+      
+      
+
+    </div>
+
+  );
+
+  
+}
+
+export default Home;
+
+
+{/* <div className="brand-picture" stylesheet={{height:"70%", width:"70%"}}>
+        <img src="./Asset 1.svg" alt='No image found/Error'/>
+        </div> */}
+
+        
+
+    {/* <div>
+          <img  src= "url(./Asset 1.svg) " alt='Error'/>
+        
+        <h1>About API</h1>
+        <p>
+          The API endpoint <b>https://superhero-search.p.rapidapi.com/api/villains</b> 
+           is a service available through RapidAPI that allows developers to get information about villain characters from comic universes such as Marvel and DC. It works by sending a GET request to the endpoint, which returns data in JSON format, making it easy to use in web applications. The API typically provides information such as the villains name, image, and other character-related details, which developers can display on websites or apps.
+        </p>
+      </div> */}
